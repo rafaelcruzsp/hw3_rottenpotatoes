926952f730bdfb5ee5030a58ba4d4795a7ae9f05
rafaelcruzsp <rafael.melaum@gmail.com>
2013-02-05 14:56:23 -0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   features/step_definitions/movie_steps.rb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,20 +1,30 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+
   movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+  end
+  assert movies_table.hashes.size == Movie.all.count
+
+  #movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-  end
-  flunk "Unimplemented"
+  #end
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+
+  titles = page.all("table#movies tbody tr td[1]").map {|t| t.text}
+  assert titles.index(e1) < titles.index(e2) 
+
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -22,6 +32,18 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+
+  rating_list.split(",").each do |field|
+    field = field.strip
+    if uncheck == "un"
+       step %Q{I uncheck "ratings_#{field}"}
+       step %Q{the "ratings_#{field}" checkbox should not be checked}
+    else
+      step %Q{I check "ratings_#{field}"}
+      step %Q{the "ratings_#{field}" checkbox should be checked}
+    end
+  end
+
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
